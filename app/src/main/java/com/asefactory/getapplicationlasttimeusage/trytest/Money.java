9cbf7fd1183f99619ae427f3c318ff13b310b148
a.evsikov <a.evsikov@weadmire.io>
2023-10-01 19:02:06 +0300

    продолжаем с тестами

--- app/src/main/java/com/asefactory/getapplicationlasttimeusage/trytest/Money.java
+++ app/src/main/java/com/asefactory/getapplicationlasttimeusage/trytest/Money.java
@@ -1,35 +1,45 @@
 package com.asefactory.getapplicationlasttimeusage.trytest;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import org.jetbrains.annotations.NotNull;
 
-public abstract class Money {
+public class Money {
 
     protected int amount;
-    protected String currency;
+    private String currency;
 
     public Money(int amount, String currency) {
         this.amount = amount;
         this.currency = currency;
     }
 
-    public abstract Money times(int multiplier);
+    @NotNull
+    public Money times(int multiplier) {
+        return new Money(amount * multiplier, currency());
+    }
 
     @Override
     public boolean equals(@Nullable Object obj) {
         Money money = (Money) obj;
-        return amount == money.amount && getClass().equals(money.getClass());
+        return amount == money.amount && currency().equals(money.currency());
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return amount + " " + currency;
     }
 
     @NotNull
     public static Money dollar(int amount) {
-        return new Dollar(amount, "USD");
+        return new Money(amount, "USD");
     }
 
     @NotNull
     public static Money franc(int amount) {
-        return new Franc(amount, "CHF");
+        return new Money(amount, "CHF");
     }
 
     public @NotNull String currency() {
